469376119d521f62d35cd16dcc53427666d02a28
stewartwatts <stewartwatts@gmail.com>
2013-09-30 16:27:04 -0400

    in Normalize.jl, create NormedData Type and begin logic to norm numerical columns

--- Normalize.jl
+++ Normalize.jl
@@ -4,13 +4,28 @@ module Normalize
 using DataFrames
 using DimensionalityReduction
 
+export NormedData
+
+type NormedData
+    X::DataFrame
+    numerical_cols = filter(c -> eltype(X[c]) <: Number, colnames(X))
+    other_cols = filter(c -> !(eltype(X[c]) <: Number), colnames(X))
+    num_X = X[:, numerical_cols]
+    oth_X = X[:, other_cols]
+end
+
 # 0. scan a dataframe for numerical features that look problematic
 #    - strongly non-Gaussian, discontinuous, heavily quantized, log-scale
 #    - give helpful warnings or suggested 
 
+
 # 1. PCA on raw data
+f1 = pca;
 
 # 2. PCA on sphered data (each dimension zero mean, unit variance)
+function f2(num_X::DataFrame)
+    
+end
 
 # 3. PCA on zero-to-one normalized data
 
