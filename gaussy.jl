109bd48348d4cb79c7841c0cf84313a28a051c11
stewartwatts <stewartwatts@gmail.com>
2013-10-07 11:58:03 -0400

    gaussy.jl: copy x before making setindex! changes and other critical refactoring

--- gaussy.jl
+++ gaussy.jl
@@ -8,68 +8,91 @@ upperindex(x::DataArray{Float64,1}) = int(length(x)/100 * rand()*14 + 1)
 # identity
 f1(x::DataArray{Float64,1}) = x
 
+# log
+f2(x::DataArray{Float64,1}) = log(x)
+
 # exponential
 f2(x::DataArray{Float64,1}) = exp(x)
 
 # number indicating missing value assigned to to 1-15% of vector
 function f3(x::DataArray{Float64,1})
-    for _ in 1:upperindex(x)
-        x[int(rand()*length(x))+1] = -999
+    y = deepcopy(x)
+    for _ in 1:upperindex(y)
+        y[int(rand()*length(y))+1] = -999
     end
-    return x
+    return y
 end
 
 # NA assigned to 1-15% of vector
 function f4(x::DataArray{Float64,1})
-    for _ in 1:upperindex(x)
-        x[int(rand()*length(x))+1] = NA
+    y = deepcopy(x)
+    for _ in 1:upperindex(y)
+        y[int(rand()*length(y))+1] = NA
     end
-    return x
+    return y
 end
 
 # upper tail discontinuous
 function f5(x::DataArray{Float64,1})
-    lowertail = int(length(x) * (1 + 5*rand()))
-    uppertail = int(length(x) * (100 - int(1 + 5*rand())))
-    lox = sort(x)[lowertail]
-    upx = sort(x)[uppertail]
-    xrng = upx-lox
-    for _ in 1:upperindex(x)
-        x[int(rand()*length(x)+1)] = upx + xrng*(1+rand())
+    y = deepcopy(x)
+    lowertail = int(length(y)/100.0 * (1 + 5*rand()))
+    uppertail = int(length(y)/100.0 * (100 - int(1 + 5*rand())))
+    loy = sort(y)[lowertail]
+    upy = sort(y)[uppertail]
+    yrng = upy-loy
+    for _ in 1:upperindex(y)
+        y[int(rand()*length(y)+1)] = upy + yrng*(1+rand())
     end
-    return x
+    return y
 end
 
 # lower tail discontinuous
 function f6(x::DataArray{Float64,1})
-    return -f5(x)
+    y = deepcopy(x)
+    lowertail = int(length(y)/100.0 * (1 + 5*rand()))
+    uppertail = int(length(y)/100.0 * (100 - int(1 + 5*rand())))
+    loy = sort(y)[lowertail]
+    upy = sort(y)[uppertail]
+    yrng = upy-loy
+    for _ in 1:upperindex(y)
+        y[int(rand()*length(y)+1)] = loy - yrng*(1+rand())
+    end
+    return y
 end
 
 # both tails disconinuous
 function f7(x::DataArray{Float64,1})
-    lowertail = int(length(x) * (1 + 5*rand()))
-    uppertail = int(length(x) * (100 - int(1 + 5*rand())))
-    lox = sort(x)[lowertail]
-    upx = sort(x)[uppertail]
-    xrng = upx-lox
-    for _ in 1:int(upperindex(x)/2)
-        x[int(rand()*length(x)+1)] = upx + xrng*(1+rand())
+    y = deepcopy(x)
+    lowertail = int(length(y)/100.0 * (1 + 5*rand()))
+    uppertail = int(length(y)/100.0 * (100 - int(1 + 5*rand())))
+    loy = sort(y)[lowertail]
+    upy = sort(y)[uppertail]
+    yrng = upy-loy
+    for _ in 1:int(upperindex(y)/2)
+        y[int(rand()*length(y)+1)] = upy + yrng*(1+rand())
     end
-    for _ in 1:int(upperindex(x)/2)
-        x[int(rand()*length(x)+1)] = lox - xrng*(1+rand())
+    for _ in 1:int(upperindex(y)/2)
+        y[int(rand()*length(y)+1)] = loy - yrng*(1+rand())
     end
-    return x
+    return y
 end
 
+# power up
 function f8(x::DataArray{Float64,1})
-    return x ^ (10*rand())
+    y = x .^ (10*rand())
+    return sum(isnan(y)) == 0 ? y : x
 end
 
+# power down
 function f9(x::DataArray{Float64,1})
-    return x ^ (1/(10*rand()))
+    y = x .^ (1/(10*rand()))
+    return sum(isnan(y)) == 0 ? y : x
 end   
 
-funcs = [f1, f2, f3, f4, f5, f6, f7, f8, f9]
+# negative power / reciprocal
+f10(x::DataArray{Float64,1}) = 1 ./ x
+
+funcs = [f1, f2, f3, f4, f5, f6, f7, f8, f9, f10]
 
 # function with metrics
 function score_gauss(df::DataFrame)
@@ -82,15 +105,15 @@ end
 # create a DataFrame with messy, un-gaussian columns
 df = DataFrame(randn(10000,8*length(funcs)));
 for i in 1:length(funcs)
-    df[string("x",i)] = funcs[i](df[string("x",i)])
+    df[i] = funcs[i](df[i])
 end
 for i in (length(funcs)+1):4*length(funcs)
     fs = sample(1:length(funcs),2)
-    df[string("x",i)] = funcs[fs[1]](funcs[fs[2]](df[string("x",i)]))
+    df[i] = funcs[fs[1]](funcs[fs[2]](df[i]))
 end
 for i in (4*length(funcs)+1):8*length(funcs)
     fs = sample(1:length(funcs),3)
-    df[string("x",i)] = funcs[fs[1]](funcs[fs[2]](funcs[fs[3]](df[string("x",i)])))
+    df[i] = funcs[fs[1]](funcs[fs[2]](funcs[fs[3]](df[i])))
 end
 
 
@@ -114,7 +137,8 @@ function g6(x::DataArray{Float64,1})
     end
     return x
 end
-function g7(x::DataArray{Float64,1})
+function g7(x::DataArray{Float64,1}, lo_cut::Int64, hi_cut::Int64)
+    # chop off vals above / below a set of quantiles
     
 end
 
