9045c7376c5a34120aba8bd076d4c2af3d545354
stewartwatts <stewartwatts@gmail.com>
2013-10-14 16:42:59 -0400

    first complete fillNA.jl; not modify in place; need testing, metrics implemented

--- fillNA.jl
+++ fillNA.jl
@@ -2,38 +2,65 @@ using DataFrames
 using RDatasets
 using GLM
 
-function clean_df!{T<:String}(df::AbstractDataFrame, exclude_colnames::Array{T,1})
+function clean_df!(df::AbstractDataFrame)
     clean_colnames!(df)
-    exclude_colnames = ["" exclude_colnames]
-    cns = filter(c -> !in(exclude_colnames, c), colnames(df))
+    
     # promotion to Float
-    num_cols = filter(c -> eltype(df[c]) <: Number, cns)
+    num_cols = filter(c -> eltype(df[c]) <: Number, colnames(df))
     for col in num_cols
-        df[col] = etype(df[col]) <: FloatingPoint ? df[col] : 1.0 * df[col]
+        df[col] = eltype(df[col]) <: FloatingPoint ? df[col] : 1.0 * df[col]
     end
+
     return
 end
 
-function optNAFill!{T<:String}(df::AbstractDataFrame, exclude_colnames::Array{T,1})
-    #optimistic NA filling
-
-    (m,n) = size(df)
-    means = Dict{Union(UTF8String,ASCIIString),Float64}()
-    stds  = Dict{Union(UTF8String,ASCIIString),Float64}()
-    fits  = Dict{Union(UTF8String,ASCIIString),LmMod}()
+function optNAFill{T<:String}(df::AbstractDataFrame, exclude_colnames::Array{T,1}; clean=true)
+    # exclude_colnames: Array of strings containing colnames of data that can't reasonably
+    #                   be inferred or created.  Ex: the response variable.  Any row with a
+    #                   missing response variable should reasonably be excluded from analysis.
     
-
-    # model each column on other features
-    for col in cols
-        fits[col] = lm(Formula(parse(string(col, " ~ ", join(filter(k -> k != col, cols), " + ")))), df)
+    if clean
+        clean_df!(df)
     end
+    df_cp = deepcopy(df)
+    
+    cols = filter(c -> !in(c, ["" exclude_colnames]) && eltype(df[c]) <: FloatingPoint, colnames(df))
+    
+    #(m,n) = size(df)
+    #fits  = Dict{Union(UTF8String,ASCIIString),LmMod}()
+    #fill_vals = Dict{Union(UTF8String,Dict{Integer,FloatingPoint}),LmMod}()
+    
+    # model each numerical column on other features
+    #for col in cols
+    #    fits[col] = lm(Formula(parse(string(col, " ~ ", join(filter(k -> k != col, cols), " + ")))), df)
+    #end
 
-    # collect col stats
+    # predict NAs
     for col in cols
-        colmean = mean(removeNA(df[col]))
-        colstd  = std(removeNA(df[col]))
-        df[col] = (df[col] - colmean) / colstd
-        means[col] = colmean
-        stsd[col] = colstd
-    end    
-end  # of function optNAfill
+        other_cols = filter(c -> c != col, cols)
+        # array of colnames with data -> indexes pertained to
+        frms = Dict()
+        preds = fill(0., sum(isna(df[col])))      # length n
+        
+        # predict() only works for Array of new data (no NA)
+        # must fit a model for each dense-Array signiture
+        ba = !isna(df[isna(df[col]), other_cols])    # n-BitArray
+        for i in 1:size(ba,1)
+            cns = other_cols[find(ba[i,:] .== true)] # colnames
+            if haskey(frms, cns)
+                frms[cns] = [frms[cns]; i]
+            else
+                frms[cns] = [i]
+            end
+        end
+
+        for frm in keys(frms)
+            fit = lm(Formula(parse(string(col, " ~ ", join(frm, " + ")))), df)
+            sub_data_arr = DataArray(df[isna(df[col]), convert(Array{ASCIIString,1}, frm)][frms[frm],:])
+            arr = convert(Array{Float64,2}, sub_data_arr)
+            preds[frms[frm]] = predict(fit, [ones(size(arr,1)) arr])
+        end
+        df_cp[isna(df[col]), col] = preds
+    end
+    return df_cp
+end  
--- test_fillNA.jl
+++ test_fillNA.jl
@@ -10,14 +10,18 @@ swiss_copy = deepcopy(swiss)
 exclude_colnames = ["Fertility"]
 
 # insert NAs to feature vectors (not into Fertility, the response variable)
-cols = filter(c -> !(c in ["" exclude_colnames]), num_cols)
+cols = filter(c -> !(c in ["" exclude_colnames]), colnames(swiss))
+(m,n) = size(swiss)
 for col in cols
     for _ in 1:10
-        df[col][ceil(rand()*m)] = NA
+        swiss[col][ceil(rand()*m)] = NA
     end
 end
 
 # do NA-filling
 optNAfill!(df, exclude_columns)
 
-# test performance  ...
+# test performance of optimistic NA-filling vs colmean filling
+num_cols = filter(c -> eltype(df[c]) <: Number, colnames(swiss))
+opt_error = 0
+colmean_error = 0
